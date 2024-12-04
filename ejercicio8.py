ab58d2d9420eeeaa0cc2afd664bf03a73e379a04
Giovanni Lucero <giovanni.lucero@genstudents.org>
2024-12-04 14:55:51 +0000

    trabajo actualizado

---
+++ ejercicio8.py
@@ -0,0 +1,16 @@
+print("Bienvenido al Mercado Digital!")
+#definir lista []
+shopping_list = []
+
+articulo_1 = input("Agregue el articulo 1: ")
+articulo_2 = input("Agregue el articulo 2: ")
+articulo_3 = input("Agregue el articulo 3: ")
+
+print(shopping_list)
+
+print("lista de compra: ")
+shopping_list.append(articulo_1) # agrega .append a lista de articulos Shopping_list
+shopping_list.append(articulo_2)
+shopping_list.append(articulo_3)
+
+print(shopping_list)
