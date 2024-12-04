6218cd86a3e7f745bff51f64784c1ac66f88021f
Giovanni Lucero <giovanni.lucero@genstudents.org>
2024-12-04 15:31:25 +0000

    trabajo actualizado

---
+++ ejemplodedesarrollopr.py
@@ -0,0 +1,7 @@
+i = [1, 2, 3]
+index = 0  # Para iterar sobre la lista
+
+while index < len(i):
+    if i[index] % 2 == 1:
+        print(i[index])  # Imprimir solo los números impares
+    index += 1  # Incremento del índice
