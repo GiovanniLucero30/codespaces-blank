ae754108e68754017ac22dfea0503a6b2f2c53c2
Giovanni Lucero <giovanni.lucero@genstudents.org>
2024-12-04 15:00:39 +0000

    trabajo actualizado

---
+++ ejercicio15.py
@@ -0,0 +1,10 @@
+# Mensaje de bienvenida
+print("¡Cuenta regresiva!")
+
+# Inicializar
+contador = 10
+
+# Bucle while  de 10 a 1
+while contador >= 1:
+    print(contador)
+    contador -= 1  # Reducir el contador en 1 en cada iteración
